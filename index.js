d412d1753cdf08f89995156bcd606b0800869f7b
fjaquet <fjaquet@outlook.fr>
2026-03-19 19:20:03 +0100

    create / route and set default http port

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ const marvelRoutes = require("./routes/marvelRoutes");
 
 app.use(marvelRoutes);
 
+app.get("/", (req, res, next) => {
+  try {
+    return res.json("Marvel backend");
+  } catch (error) {
+    next(error);
+  }
+});
+
 app.all(/.*/, (req, res) => {
   return res.status(404).json({ message: "Page not found" });
 });
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server started");
 });
